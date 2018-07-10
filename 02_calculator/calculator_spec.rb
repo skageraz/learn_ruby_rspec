0f5812e4efb7f1d5d7dc5f0776d1fd9860bf4c96
skageraz <39991372+skageraz@users.noreply.github.com>
2018-07-10 10:49:41 +0200

    Update calculator_spec.rb

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -82,12 +82,14 @@ describe "#multiply" do
     expect(multiply(3, 4)).to eq(12)
   end
 
-  it "multiplies two other numbers"
-    # one other test here, don't forget do end ;)
-
-  it "multiplies two other numbers, one of them being 0"
-    # one last test, with 0 in it
-
+ it “multiplies two other numbers”do
+   expect(multiply(7, 8)).to eq(56)
+ end
+ 
+ it “multiplies two other numbers, one of them being 0" do
+   expect(multiply(7, 0)).to eq(0)
+ end
+end
 end
 
 describe "#power" do
